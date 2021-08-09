fcf7a506d928f782864ee1eaf9dd0608d60421a8
Henry Parras <henry.parras@yahoo.com>
2021-08-09 13:56:36 -0700

    added hero and player pics

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function displayDodgers(data) {
                     
     winsEl.innerHTML = `Wins: ${data.response.games.wins.all.total}`;
     losesEl.innerHTML = `Loses: ${data.response.games.loses.all.total}`;
-    logoEl.innerHTML = "<img src='https://media.api-sports.io/baseball/teams/18.png'>";
+    logoEl.innerHTML = `<img src="https://i.ibb.co/ss1Z6H1/dodgers-world-series-video.jpg" width="100%">`;
     // momentoEl.innerHTML =  `${momento}`;
     // chosenCityEl.innerHTML = `${city}`;
 
@@ -85,6 +85,10 @@ function displayMookie(data){
     mookieBody.classList.add('text-white');
     mookieEl.append(mookieBody);
 
+    var mookiePic = document.createElement('div');
+    mookiePic.innerHTML = `<img src="https://i.ibb.co/tZfbSkS/mookie-betts-6.png" width="100%">`;
+    mookieBody.appendChild(mookiePic);
+
     var mookieName = document.createElement('div');
     mookieName.innerHTML = `Mookie Betts`;
     mookieBody.appendChild(mookieName);
@@ -93,9 +97,21 @@ function displayMookie(data){
     mookieAvg.innerHTML = `Avg: ${data.proj_pecota_batting.queryResults.row.avg}`;
     mookieBody.appendChild(mookieAvg);
 
+    var mookieOps = document.createElement('div');
+    mookieOps.innerHTML = `OPS: ${data.proj_pecota_batting.queryResults.row.ops}`;
+    mookieBody.appendChild(mookieOps);
+
     var mookieHr = document.createElement('div');
     mookieHr.innerHTML = `HR: ${data.proj_pecota_batting.queryResults.row.hr}`;
     mookieBody.appendChild(mookieHr);
+
+    var mookieRbi = document.createElement('div');
+    mookieRbi.innerHTML = `RBI: ${data.proj_pecota_batting.queryResults.row.rbi}`;
+    mookieBody.appendChild(mookieRbi);
+
+    var mookieR = document.createElement('div');
+    mookieR.innerHTML = `R: ${data.proj_pecota_batting.queryResults.row.r}`;
+    mookieBody.appendChild(mookieR);
 }
 
 
@@ -135,6 +151,10 @@ function displayCt(data){
     ctBody.classList.add('text-white');
     ctEl.append(ctBody);
 
+    var ctPic = document.createElement('div');
+    ctPic.innerHTML = `<img src="https://i.ibb.co/nLtWrJr/ctslide.jpg" width="100%">`;
+    ctBody.appendChild(ctPic);
+
     var ctName = document.createElement('div');
     ctName.innerHTML = `Chris Taylor`;
     ctBody.appendChild(ctName);
@@ -143,9 +163,21 @@ function displayCt(data){
     ctAvg.innerHTML = `Avg: ${data.proj_pecota_batting.queryResults.row.avg}`;
     ctBody.appendChild(ctAvg);
 
+    var ctOps = document.createElement('div');
+    ctOps.innerHTML = `OPS: ${data.proj_pecota_batting.queryResults.row.ops}`;
+    ctBody.appendChild(ctOps);
+
     var ctHr = document.createElement('div');
     ctHr.innerHTML = `HR: ${data.proj_pecota_batting.queryResults.row.hr}`;
     ctBody.appendChild(ctHr);
+
+    var ctRbi = document.createElement('div');
+    ctRbi.innerHTML = `RBI: ${data.proj_pecota_batting.queryResults.row.rbi}`;
+    ctBody.appendChild(ctRbi);
+
+    var ctR = document.createElement('div');
+    ctR.innerHTML = `R: ${data.proj_pecota_batting.queryResults.row.r}`;
+    ctBody.appendChild(ctR);
 }
 
 function getJt(){
@@ -178,6 +210,10 @@ function displayJt(data){
     jtBody.classList.add('text-white');
     jtEl.append(jtBody);
 
+    var jtPic = document.createElement('div');
+    jtPic.innerHTML = `<img src="https://i.ibb.co/8Xs7XB7/justinturnerarticlepic.jpg" width="100%">`;
+    jtBody.appendChild(jtPic);
+
     var jtName = document.createElement('div');
     jtName.innerHTML = `Justin Turner`;
     jtBody.appendChild(jtName);
@@ -186,7 +222,19 @@ function displayJt(data){
     jtAvg.innerHTML = `Avg: ${data.proj_pecota_batting.queryResults.row.avg}`;
     jtBody.appendChild(jtAvg);
 
+    var jtOps = document.createElement('div');
+    jtOps.innerHTML = `OPS: ${data.proj_pecota_batting.queryResults.row.ops}`;
+    jtBody.appendChild(jtOps);
+
     var jtHr = document.createElement('div');
     jtHr.innerHTML = `HR: ${data.proj_pecota_batting.queryResults.row.hr}`;
     jtBody.appendChild(jtHr);
+
+    var jtRbi = document.createElement('div');
+    jtRbi.innerHTML = `RBI: ${data.proj_pecota_batting.queryResults.row.rbi}`;
+    jtBody.appendChild(jtRbi);
+
+    var jtR = document.createElement('div');
+    jtR.innerHTML = `R: ${data.proj_pecota_batting.queryResults.row.r}`;
+    jtBody.appendChild(jtR);
 }
